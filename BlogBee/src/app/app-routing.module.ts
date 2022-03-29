968bfdbadf73842b89ada20f1f4ed8b643ef8bfe
Muktar16 <bsse1116@iit.du.ac.bd>
2022-03-30 01:41:07 +0600

    nav bar and home

--- BlogBee/src/app/app-routing.module.ts
+++ BlogBee/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'signin',component:SignInComponent},
+  {path:'navbar',component:NavbarComponent},
+  {path:'signup',component:SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BlogBee/src/app/app.module.ts
+++ BlogBee/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HomeComponent,
+    SignInComponent,
+    NavbarComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
